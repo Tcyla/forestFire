c285e3a30ccc5ad12290071c140499c0d999ed98
Tcyla <51529038+Tcyla@users.noreply.github.com>
2021-12-10 17:13:19 +0100

    Add files via upload

---
+++ headers/ForestFire.h
@@ -0,0 +1,58 @@
+#ifndef __FORESTFIRE__
+#define __FORESTFIRE__
+
+#include<iostream>
+#include <SDL2/SDL.h>
+
+#include "StateMatrix.h"
+
+
+class ForestFire
+{
+    private:
+        StateMatrix* M_matrix;
+		StateMatrix* M_matrixBuffer;
+        int M_curentTime = 0;
+        int M_row;
+        int M_col;
+	
+		char get_torified_coeffs( int , int );
+		
+		char rule( int,  int );
+
+		void swap_buffer();
+	
+    public:
+
+		ForestFire( int, int );
+		~ForestFire()
+		{
+			delete M_matrix; 
+			delete M_matrixBuffer;
+		}
+
+		friend std::ostream& operator<<(std::ostream&, ForestFire&);
+
+		// Getters
+		int row(){ return M_row; }
+		int col(){ return M_col; }
+		int currentTime() const { return M_curentTime; }
+		void getState(SDL_Rect, int,
+					  SDL_Rect*, int&,
+					  SDL_Rect*, int&,
+					  SDL_Rect*, int&); 
+		
+		// Setters
+		void setFire(int, int);
+		void step();
+		
+		void run(int);
+		void reset();
+
+		StateMatrix matrix() {return *M_matrix;}
+
+};
+
+std::ostream& operator<<( std::ostream&, ForestFire& );
+
+#endif
